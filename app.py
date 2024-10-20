ccfc3e55bfbdaba99c083f0c221be15d348ab6f2
fitszimoon <fitszimoon@gmail.com>
2024-10-20 16:22:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/joker')
+def joker_info():
+    return render_template('joker.html')
+
+@app.route('/morgana')
+def morgana_info():
+    return render_template('morgana.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
